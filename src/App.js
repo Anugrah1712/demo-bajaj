368297c15b42e82127e894d8359c26defb333554
Anugrah <anugrah@Anugrahs-MacBook-Air.local>
2025-06-28 17:55:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Chatbot } from 'rag-chatbot-ui-bajaj';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to Bajaj-Finance</h2>
+      <Chatbot />
     </div>
   );
 }
